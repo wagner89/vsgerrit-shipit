e13b1e9c0e2cc7b530d4a7b3e9c24f9792be0e39
Tamas Wagner <tamas.wagner@gmail.com>
2015-12-09 11:53:12 +0200

    Add ButtonBarViewModel
    Change-Id: I1c0f6196422e52c27b75788026cf51d1bd6c7853

---
+++ src/VSGerrit/Features/ChangeBrowser/Controls/ButtonBar/ButtonBarViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+using System.Windows.Input;
+
+namespace VSGerrit.Features.ChangeBrowser.Controls.ButtonBar
+{
+    public class ButtonBarViewModel
+    {
+        public ButtonBarViewModel()
+        {
+            SettingsButtonClickCommand = new DelegateCommand(_ => ToggleSettingsView(), (_) => true);
+        }
+
+        public ICommand SettingsButtonClickCommand { get; set; }
+
+        private void ToggleSettingsView()
+        {
+            // TODO
+        }
+    }
+}
